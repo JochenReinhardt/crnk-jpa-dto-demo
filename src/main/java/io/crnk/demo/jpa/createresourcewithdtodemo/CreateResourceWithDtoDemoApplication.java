bec00699e104529a127e166ae604f01ab09b1f85
Jochen Reinhardt <jochen.reinhardt@exxeta.de>
2019-01-15 09:03:19 +0100

    Spring Boot initialized

---
+++ src/main/java/io/crnk/demo/jpa/createresourcewithdtodemo/CreateResourceWithDtoDemoApplication.java
@@ -0,0 +1,14 @@
+package io.crnk.demo.jpa.createresourcewithdtodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateResourceWithDtoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateResourceWithDtoDemoApplication.class, args);
+	}
+
+}
+
