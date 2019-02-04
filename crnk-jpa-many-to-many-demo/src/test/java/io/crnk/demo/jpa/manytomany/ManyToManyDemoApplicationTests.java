ff092309c2337bbd99ddd360bb3617bd25dd8429
Jochen Reinhardt <jochen.reinhardt@exxeta.com>
2019-02-04 14:36:47 +0100

    cleanup

--- crnk-jpa-many-to-many-demo/src/test/java/io/crnk/demo/jpa/manytomany/ManyToManyDemoApplicationTests.java
+++ crnk-jpa-many-to-many-demo/src/test/java/io/crnk/demo/jpa/manytomany/ManyToManyDemoApplicationTests.java
@@ -1,6 +1,8 @@
 package io.crnk.demo.jpa.manytomany;
 
 import io.crnk.client.CrnkClient;
+import io.crnk.core.queryspec.IncludeRelationSpec;
+import io.crnk.core.queryspec.PathSpec;
 import io.crnk.core.queryspec.QuerySpec;
 import io.crnk.core.repository.ResourceRepositoryV2;
 import io.crnk.core.resource.list.ResourceList;
@@ -8,6 +10,7 @@ import io.crnk.demo.jpa.manytomany.left.LeftDTO;
 import io.crnk.demo.jpa.manytomany.right.RightDTO;
 import io.crnk.spring.client.RestTemplateAdapter;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +18,7 @@ import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.Serializable;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -31,6 +35,7 @@ public class ManyToManyDemoApplicationTests {
         ((RestTemplateAdapter) client.getHttpAdapter()).getImplementation().setRequestFactory(new OkHttp3ClientHttpRequestFactory());
     }
 
+    @Test
     public void testGetLefts() {
         ResourceRepositoryV2<LeftDTO, Serializable> leftRepository = client.getRepositoryForType(LeftDTO.class);
         ResourceList<LeftDTO> lefts = leftRepository.findAll(new QuerySpec(LeftDTO.class));
@@ -38,6 +43,7 @@ public class ManyToManyDemoApplicationTests {
         assertEquals(10, lefts.size());
     }
 
+    @Test
     public void testGetRights() {
         ResourceRepositoryV2<RightDTO, Serializable> leftRepository = client.getRepositoryForType(RightDTO.class);
         ResourceList<RightDTO> rights = leftRepository.findAll(new QuerySpec(RightDTO.class));
@@ -45,5 +51,19 @@ public class ManyToManyDemoApplicationTests {
         assertEquals(10, rights.size());
     }
 
-
+    @Test
+    public void testGetLeftsIncludeRights() {
+        ResourceRepositoryV2<LeftDTO, Serializable> leftRepository = client.getRepositoryForType(LeftDTO.class);
+        QuerySpec querySpec = new QuerySpec(LeftDTO.class);
+        querySpec.getIncludedRelations().add(new IncludeRelationSpec(PathSpec.of("rights")));
+        ResourceList<LeftDTO> lefts = leftRepository.findAll(querySpec);
+        assertNotNull(lefts);
+        assertEquals(10, lefts.size());
+        for (int i = 0; i < 10; i++) {
+            LeftDTO left = lefts.get(i);
+            List<RightDTO> rights = left.getRights();
+            assertNotNull(rights);
+            assertEquals(i + 1, rights.size());
+        }
+    }
 }
